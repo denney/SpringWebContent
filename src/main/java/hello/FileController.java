40580b5d472730098fb8bfe9400b2847a061f0e2
dst <695493589@qq.com>
2017-06-12 01:28:29 +0800

    图片和表单一起提交测试

--- src/main/java/hello/FileController.java
+++ src/main/java/hello/FileController.java
@@ -1,4 +1,7 @@
 package hello;
+
+import domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,25 +12,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class FileController {
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+    @Value("${imgPath.Prefix}")
+    private String filepath1;
+
+    public static Map<String, Object> resultJson(int resultCode, String messageValue, Object objectValue) {
+        Map map = new HashMap();
+        map.put("resultCode", resultCode);
+        map.put("message", messageValue);
+        map.put("data", objectValue);
+        return map;
+    }
+
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
     //文件上传相关代码
     @RequestMapping(value = "upload")
     @ResponseBody
-    public String upload(@RequestParam("test") MultipartFile file) {
+    public Map upload(@RequestParam("test") MultipartFile file, User user) {
+        System.out.println("类======" + user);
+
+            user.setImgPath(filepath1);
+
+
         if (file.isEmpty()) {
-            return "文件为空";
+            return resultJson(102, "文件为空", user);
         }
         // 获取文件名
         String fileName = file.getOriginalFilename();
@@ -36,7 +59,7 @@ public class FileController {
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         logger.info("上传的后缀名为：" + suffixName);
         // 文件上传后的路径
-        String filePath = "E://test//";
+        String filePath = "/Users/admin/Desktop/a/";
         // 解决中文问题，liunx下中文路径，图片显示问题
         // fileName = UUID.randomUUID() + suffixName;
         File dest = new File(filePath + fileName);
@@ -46,18 +69,26 @@ public class FileController {
         }
         try {
             file.transferTo(dest);
-            return "上传成功";
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        } catch (Exception e) {
+            return resultJson(101, "上传图片失败", user);
         }
-        return "上传失败";
+
+
+        try {
+//          User user= userService.save(user);
+            user.setId(1L);
+        } catch (Exception e) {
+            return resultJson(103, "图片上传成功，数据库连接异常", user);
+        }
+
+
+        return resultJson(100, "图片上传成功，数据保存成功", user);
     }
 
     //文件下载相关代码
     @RequestMapping("/download")
-    public String downloadFile(org.apache.catalina.servlet4preview.http.HttpServletRequest request, HttpServletResponse response){
+    public String downloadFile(org.apache.catalina.servlet4preview.http.HttpServletRequest request, HttpServletResponse response) {
         String fileName = "FileUploadTests.java";
         if (fileName != null) {
             //当前是从该工程的WEB-INF//File//下获取文件(该目录可以在下面一行代码配置)然后下载到C:\\users\\downloads即本机的默认下载的目录
@@ -67,7 +98,7 @@ public class FileController {
             if (file.exists()) {
                 response.setContentType("application/force-download");// 设置强制下载不打开
                 response.addHeader("Content-Disposition",
-                        "attachment;fileName=" +  fileName);// 设置文件名
+                        "attachment;fileName=" + fileName);// 设置文件名
                 byte[] buffer = new byte[1024];
                 FileInputStream fis = null;
                 BufferedInputStream bis = null;
@@ -103,6 +134,7 @@ public class FileController {
         }
         return null;
     }
+
     //多文件上传方法一  下载到该工程的所在目录下
     @RequestMapping(value = "/batch/upload", method = RequestMethod.POST)
     @ResponseBody
@@ -133,12 +165,12 @@ public class FileController {
         }
         return "upload successful";
     }
+
     //多文件上传方法二
     @RequestMapping(value = "/batchupload", method = RequestMethod.POST)
     @ResponseBody
-    public String batchFileUp(HttpServletRequest request)
-    {
-        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
+    public String batchFileUp(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
         //String contextPath = request.getSession().getServletContext().getRealPath("/")+ "\\uploadsTest";//可以将文件存放在这个目录下  是当前工程下的uploadtest目录下
 //        System.out.println(contextPath);
 //        File tempFile = new File(contextPath);
@@ -146,28 +178,23 @@ public class FileController {
 //        {
 //            tempFile.mkdir();
 //        }
-        for(int i=0 ;i<files.size();i++)
-        {
+        for (int i = 0; i < files.size(); i++) {
             MultipartFile file = files.get(i);
             System.out.println(file.getContentType());
             String fileName = file.getOriginalFilename();
             String suffixName = fileName.substring(fileName.lastIndexOf("."));
-            String fullfilePath = "E://test//" +fileName;//自己设定的文件目录
-            logger.info("fullfilePath:",fullfilePath);
-            if(!file.isEmpty())
-            {
-                try{
-                    BufferedOutputStream stream =new BufferedOutputStream(new FileOutputStream(new File(fullfilePath)));
+            String fullfilePath = "E://test//" + fileName;//自己设定的文件目录
+            logger.info("fullfilePath:", fullfilePath);
+            if (!file.isEmpty()) {
+                try {
+                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(fullfilePath)));
                     stream.write(file.getBytes());
                     stream.close();
+                } catch (Exception e) {
+                    return "Failed to Upload" + fileName + "=>" + e.getMessage();
                 }
-                catch (Exception e){
-                    return  "Failed to Upload"+fileName +"=>"+e.getMessage();
-                }
-            }
-            else
-            {
-                return "Failed to Upload "+ fileName +"because the file was empty";
+            } else {
+                return "Failed to Upload " + fileName + "because the file was empty";
             }
 
         }
